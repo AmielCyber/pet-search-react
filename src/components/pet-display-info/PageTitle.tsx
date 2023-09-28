fbfa1a1799fbf41a5aececd256cdd3892b6647ee
Amiel Nava <amieln@icloud.com>
2023-09-27 19:22:13 -0700

    Decode HTML entities for pet name
    Decode HTML entity and protect from XSS  from pet name since it is a user entered input.

--- src/components/pet-display-info/PageTitle.tsx
+++ src/components/pet-display-info/PageTitle.tsx
@@ -1,8 +1,9 @@
 import { Typography } from "@mui/material";
+import {decodeHtmlEntityString} from "../../utils/decodeHtmlEntity.ts";
 
 type Props = {
   name: string;
 };
 export default function PageTitle(props: Props) {
-  return <Typography variant="h2">{props.name}</Typography>;
+  return <Typography variant="h2">{decodeHtmlEntityString(props.name)}</Typography>;
 }
